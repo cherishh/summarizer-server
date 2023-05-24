8ad7bd2b928736d1ef4831d010b85f6f3589e8f9
cherishh <wzx1991@gmail.com>
2023-05-24 21:24:40 +0800

    feat: update

--- server.js
+++ server.js
@@ -32,52 +32,52 @@ const systemPrompt = {
 
 async function callChatGPTAPI(content) {
   try {
-    // const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Authorization': `Bearer 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`
-    //   },
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     model: 'gpt-3.5-turbo',
-    //     messages: [
-    //       {
-    //         role: 'system',
-    //         content: systemPrompt,
-    //       },
-    //       {
-    //         role: 'user',
-    //         // content: `请帮助我总结下面包裹在'''内的这篇文章的主要内容。
-    //         // '''${content}'''
-    //         // `
-    //         content: '你好的英文是什么'
-    //       }
-    //     ],
-    //     max_tokens: 1000,
-    //     temperature: 1,
-    //     stream: false,
-    //   }),
-    // });
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-      model: 'gpt-3.5-turbo',
-      messages: [
-        systemPrompt,
-        {
-          role: 'user',
-          // content: `请帮助我总结下面包裹在'''内的这篇文章的主要内容。
-          // '''${'there are many apps like Postman that you can use to test your API. Some popular alternatives include: Insomnia Paw Thunder Client SoapUI ReadyAPI Testsigma Katalon Studio SoapUI Pro These tools offer a variety of features, such as the ability to send and receive requests, view response headers and bodies, and create and manage collections of requests. They can be used to test APIs of all types, including RESTful APIs, SOAP APIs, and GraphQL APIs. When choosing an API testing tool, it is important to consider your specific needs and requirements. Some factors to consider include the features offered by the tool, the price, and the ease of use. If you are looking for a free and open-source API testing tool, Postman is a good option. It is easy to use and offers a variety of features, including the ability to send and receive requests, view response headers and bodies, and create and manage collections of requests. If you are looking for a more powerful API testing tool, there are a number of commercial options available. These tools offer a wider range of features and can be used to test more complex APIs. However, they can be more expensive than free and open-source tools. No matter which API testing tool you choose, it is important to test your APIs regularly to ensure that they are working properly. This will help to avoid problems and improve the quality of your API.'}'''
-          // `
-          content: '你好的英文是什么'
-        }
-      ],
-      max_tokens: 1000,
-      temperature: 1,
-      stream: false,
-    }, {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
       headers: {
-        'Authorization': 'Bearer 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'
-      }
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer 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`
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content: systemPrompt,
+          },
+          {
+            role: 'user',
+            // content: `请帮助我总结下面包裹在'''内的这篇文章的主要内容。
+            // '''${content}'''
+            // `
+            content: '你好的英文是什么'
+          }
+        ],
+        max_tokens: 1000,
+        temperature: 1,
+        stream: false,
+      }),
     });
+    // const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    //   model: 'gpt-3.5-turbo',
+    //   messages: [
+    //     systemPrompt,
+    //     {
+    //       role: 'user',
+    //       // content: `请帮助我总结下面包裹在'''内的这篇文章的主要内容。
+    //       // '''${'there are many apps like Postman that you can use to test your API. Some popular alternatives include: Insomnia Paw Thunder Client SoapUI ReadyAPI Testsigma Katalon Studio SoapUI Pro These tools offer a variety of features, such as the ability to send and receive requests, view response headers and bodies, and create and manage collections of requests. They can be used to test APIs of all types, including RESTful APIs, SOAP APIs, and GraphQL APIs. When choosing an API testing tool, it is important to consider your specific needs and requirements. Some factors to consider include the features offered by the tool, the price, and the ease of use. If you are looking for a free and open-source API testing tool, Postman is a good option. It is easy to use and offers a variety of features, including the ability to send and receive requests, view response headers and bodies, and create and manage collections of requests. If you are looking for a more powerful API testing tool, there are a number of commercial options available. These tools offer a wider range of features and can be used to test more complex APIs. However, they can be more expensive than free and open-source tools. No matter which API testing tool you choose, it is important to test your APIs regularly to ensure that they are working properly. This will help to avoid problems and improve the quality of your API.'}'''
+    //       // `
+    //       content: '你好的英文是什么'
+    //     }
+    //   ],
+    //   max_tokens: 1000,
+    //   temperature: 1,
+    //   stream: false,
+    // }, {
+    //   headers: {
+    //     'Authorization': 'Bearer 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'
+    //   }
+    // });
 
     if (response.status === 200 && response.data && response.data.choices && response.data.choices.length > 0) {
       console.log(response.data.choices[0].message, 'msg');
